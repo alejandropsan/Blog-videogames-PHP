8fad6c0fd5e44f9b576908a930a48cd0b1781bfe
alejandropsan <alejandropsan1@gmail.com>
2022-11-19 07:01:31 +0100

    Incorporación nueva estructura

---
+++ includes/pie.php
@@ -0,0 +1,13 @@
+<?php include_once 'pie.php'; ?>
+
+    <div class="clearfix"></div>
+    </div> <!-- Fin principal -->
+
+        <footer id="pie">
+            <p>Desarrollado por Alejandropsan &copy; 2022</p>
+        </footer>
+        
+    </body>
+        
+</html>
+
